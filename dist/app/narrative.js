40701e910e370d745b4089c551dc38b767a1eb4d
Mark Rudolph <mark_rudolph@yahoo.com>
2024-04-30 21:07:31 -0700

    expt - import with importmap defining three and three/addons/

--- dist/app/narrative.js
+++ dist/app/narrative.js
@@ -14,11 +14,12 @@
 // Three.js
 
 //EXPT !!!!!!!!!!!!!
-import * as THREE from '../../../../node_modules/three/build/three.module.js';
-//import * as THREE from '../node_modules/three/build/three.module.js';
+//import * as THREE from '../../../node_modules/three/build/three.module.js';
+import * as THREE from 'three';
+
 //import * as THREE from '../external/three/build/three.module.js';
 
-import { VRButton } from '../../../../node_modules/three/examples/jsm/webxr/VRButton.js';
+import { VRButton } from 'three/addons/';
 //import { VRButton } from '../../../node_modules/three/examples/jsm/webxr/VRButton.js';
 //import {VRButton} from '../external/three/examples/jsm/webxr/VRButton.js';
 //EXPT !!!!!!!!!!!!!
