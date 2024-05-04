e902485d65d6f096d515a0be657e4ad3e74965a1
Mark Rudolph <mark_rudolph@yahoo.com>
2024-05-03 19:29:17 -0700

    fixed quad_fsh/fsh_rm_expt1.glasl.js

--- dist/app/models/stage/shaders/webgl1/quad_fsh/fsh_rm_expt1.glsl.js
+++ dist/app/models/stage/shaders/webgl1/quad_fsh/fsh_rm_expt1.glsl.js
@@ -1,7 +1,8 @@
 // fsh_rm_expt1.glsl.ts
 // fragment shader
 // raymarch - expt1-fogcubes
-import * as THREE from '../../../../../../../node_modules/three/build/three.module.js';
+//import * as THREE from '../../../../../../../node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 const uniforms = {
     tDiffuse: { type: 't', value: null },
     uVertex: { type: 'v3', value: new THREE.Vector3() },
